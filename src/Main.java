a857df7812cd99b5474b28938e70b8dced1cb4b1
Rédouane Drici <redouane.drici@divtec.ch>
2024-09-12 15:24:56 +0200

    Ajout de la grande et de la petite suite + ajout de l'affichage du début de jeu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,6 +12,10 @@ public class Main {
     private static final int NBR_DES_BRELAN = 3;
     private static final int NBR_DES_CARRE = 5;
     private static final int NBR_DES_FULL = 4;
+
+    private static final String RED = "\u001B[31m";
+    private static final String BLUE = "\u001B[34m";
+    private static final String RESET = "\u001B[0m";
 
     /**
      * Prend un nombre aléatoire entre 1 et le nombre donné (simulation d'un dé)
@@ -24,7 +29,7 @@ public class Main {
 
     /**
      * Prend plusieurs nombres aléatoire en fonction du nombre donné en paramètre (simulation de plusieurs dés lancé)
-     * @param nbrDes Le nombre de chiffre à tirer
+     * @param nbrDes   Le nombre de chiffre à tirer
      * @param nbrFaces le nombre max que l'on peut tirer
      * @return un tableau avec les valeurs des nombres aléatoires obtenus
      */
@@ -100,8 +105,8 @@ public class Main {
      * @return le/les numéro(s) des dés à relancer
      */
     private static String[] retourneValeurEcrit() {
-        String reponseDesRelance = demandeUneReponse("Quelles dés voulez-vous relancer [1 à " + NOMBRE_FACES
-                        + "] ? Séparez les par des espaces");
+        String reponseDesRelance = demandeUneReponse("Quelles dés voulez-vous relancer [1 à " + NOMBRE_DES
+                + "] ? Séparez les par des espaces");
         String[] values = reponseDesRelance.split(" ");
         return values;
     }
@@ -109,8 +114,8 @@ public class Main {
     /**
      * Demande la confirmation de l'utlisateur de relancer les dés et les quelles
      * @param tableauEntiers le tableau contenant des valeurs entières
-     * @param nbrJetDe le nombre de tours disponible
-     * @param nbrFaces le nombre max que l'on peut tirer
+     * @param nbrJetDe       le nombre de tours disponible
+     * @param nbrFaces       le nombre max que l'on peut tirer
      */
     private static void relancerDes(int[] tableauEntiers, int nbrJetDe, int nbrFaces) {
         int nbrLancer = 1;
@@ -146,15 +151,15 @@ public class Main {
     /**
      * Calcule le nombre de des de même valeur dans un tableau de dés donné
      * @param tableauDes le tableau contenant les valeurs des dés
-     * @param nbrDes Le nombre de chiffre à tirer
+     * @param nbrDes     Le nombre de chiffre à tirer
      * @return le nombre de dés identique
      */
-    private static int nbrDesIdentique (int[] tableauDes, int nbrDes) {
+    private static int nbrDesIdentique(int[] tableauDes, int nbrDes) {
         int desIdentique = 0;
         for (int indexActuel = 0; indexActuel < nbrDes; indexActuel++) {
             for (int indexSuivants = indexActuel + 1; indexSuivants < nbrDes; indexSuivants++) {
                 if (tableauDes[indexActuel] == tableauDes[indexSuivants]) {
-                        desIdentique++;
+                    desIdentique++;
                 }
             }
         }
@@ -164,7 +169,7 @@ public class Main {
     /**
      * vérifie si le résultats de nos lancés de dés sont Brelan (3 dés de même valeurs)
      * @param tableauDes le tableau contenant les valeurs des dés
-     * @param nbrDes Le nombre de chiffre à tirer
+     * @param nbrDes     Le nombre de chiffre à tirer
      * @return s'il y a 3 dés de même la valeurs ou non ("Brelan")
      */
     private static boolean estBrelan(int[] tableauDes, int nbrDes) {
@@ -174,7 +179,7 @@ public class Main {
     /**
      * vérifie si le résultats de nos lancés de dés sont Carre (4 dés de même valeurs)
      * @param tableauDes le tableau contenant les valeurs des dés
-     * @param nbrDes Le nombre de chiffre à tirer
+     * @param nbrDes     Le nombre de chiffre à tirer
      * @return s'il y a 4 dés de la même valeurs ou non ("Carre")
      */
     private static boolean estCarre(int[] tableauDes, int nbrDes) {
@@ -184,15 +189,64 @@ public class Main {
     /**
      * vérifie si le résultats de nos lancés de dés sont Full (un brelan + 2 dés de la même valeurs)
      * @param tableauDes le tableau contenant les valeurs des dés
-     * @param nbrDes Le nombre de chiffre à tirer
+     * @param nbrDes     Le nombre de chiffre à tirer
      * @return s'il y a un brelan + 2 dés de la même valeurs ou non ("Full")
      */
     private static boolean estFull(int[] tableauDes, int nbrDes) {
         return (nbrDesIdentique(tableauDes, nbrDes) == NBR_DES_FULL);
     }
 
-    public static void main(String[] args) {
+    /**
+     * vérifie si le résultats de nos lancés de dés sont une petite suite
+     * @param tableauDes le tableau contenant les valeurs des dés
+     * @return s'il y a une petite suite ou non
+     */
+    private static boolean estPetiteSuite(int[] tableauDes) {
+        Arrays.sort(tableauDes);
+        return (tableauDes[0] + 1 == tableauDes[1] &&
+                tableauDes[1] + 1 == tableauDes[2] &&
+                tableauDes[2] + 1 == tableauDes[3])
+                ||
+                (tableauDes[1] + 1 == tableauDes[2] &&
+                        tableauDes[2] + 1 == tableauDes[3] &&
+                        tableauDes[3] + 1 == tableauDes[4]);
+    }
 
+    /**
+     * vérifie si le résultats de nos lancés de dés sont une grande suite
+     * @param tableauDes le tableau contenant les valeurs des dés
+     * @return s'il y a une grande suite ou non
+     */
+    private static boolean estGrandeSuite(int[] tableauDes) {
+
+        Arrays.sort(tableauDes);
+        return tableauDes[0] + 1 == tableauDes[1] &&
+                tableauDes[1] + 1 == tableauDes[2] &&
+                tableauDes[2] + 1 == tableauDes[3] &&
+                tableauDes[3] + 1 == tableauDes[4];
+    }
+
+    private static String motEnPlusieurCouleur(String motSansCouleur) {
+        String motEnCouleur = "";
+        for (int i = 0; i < motSansCouleur.length(); i++) {
+            if (i % 2 == 0) {
+                motEnCouleur += RED + motSansCouleur.charAt(i);
+            } else {
+                motEnCouleur += BLUE + motSansCouleur.charAt(i);
+            }
+        }
+        return motEnCouleur;
+    }
+
+    private static void afficheDebutJeu() {
+        System.out.println(motEnPlusieurCouleur("Jeu du Yahtzee") + RESET);
+        Scanner debutJeu = new Scanner(System.in);
+        System.out.println("Appuyer sur entrée pour lancer le jeu ...");
+        debutJeu.nextLine();
+    }
+
+    public static void main(String[] args) {
+        afficheDebutJeu();
         // Premier Jet de dés
         int[] tabDes = lancerXDe(NOMBRE_DES, NOMBRE_FACES);
         affichageComplet(tabDes);
@@ -206,6 +260,10 @@ public class Main {
             System.out.println("!!!!!!! Carre !!!!!!!");
         } else if (estBrelan(tabDes, NOMBRE_DES)) {
             System.out.println("!!!!!!! Brelan !!!!!!!");
+        } else if (estGrandeSuite(tabDes)) {
+            System.out.println("!!!!!!! est grande suite !!!!!!!");
+        } else if (estPetiteSuite(tabDes)) {
+            System.out.println("!!!!!!! est petite suite !!!!!!!");
         }
     }
 }
