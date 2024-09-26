54f2298904026d32f512e72651f94c5c4895cbc7
Rédouane Drici <redouane.drici@divtec.ch>
2024-09-26 13:22:29 +0200

    ENlever la classe du résultat et l'ajouter au main

--- src/Main.java
+++ src/Main.java
@@ -111,6 +111,54 @@ public class Main {
         debutJeu.nextLine();
     }
 
+    private static int resultatPoint(Combinaison combi) {
+        int nbrPoints = 0;
+        int[] faceIdentique = new int[1];
+
+        System.out.print("Vous avez eu un");
+        if (combi.estYams() && !combi.combinaisonDejaObtenu[0]) {
+            System.out.print(" Yams.");
+            nbrPoints = NBR_POINT_YAMS;
+            combi.combinaisonDejaObtenu[0] = true;
+
+        } else if (combi.estFull() && !combi.combinaisonDejaObtenu[1]) {
+            System.out.print(" Full.");
+            nbrPoints = NBR_POINT_FULL;
+            combi.combinaisonDejaObtenu[1] = true;
+
+        } else if (combi.estCarre() && !combi.combinaisonDejaObtenu[2]) {
+            System.out.print(" Carre.");
+            nbrPoints = faceIdentique[0] * NBR_DES_CARRE;
+            combi.combinaisonDejaObtenu[2] = true;
+
+        } else if (combi.estBrelan() && !combi.combinaisonDejaObtenu[3]) {
+            System.out.print(" Brelan.");
+            nbrPoints = faceIdentique[0] * NBR_DES_BRELAN;
+            combi.combinaisonDejaObtenu[3] = true;
+
+        } else if (combi.estGrandeSuite() && !combi.combinaisonDejaObtenu[4]) {
+            System.out.print("e grande suite.");
+            nbrPoints = NBR_POINT_GRANDE_SUITE;
+            combi.combinaisonDejaObtenu[4] = true;
+
+        } else if (combi.estPetiteSuite() && !combi.combinaisonDejaObtenu[5]) {
+            System.out.print("e petite suite.");
+            nbrPoints = NBR_POINT_PETITE_SUITE;
+            combi.combinaisonDejaObtenu[5] = true;
+
+        } else if (!combi.combinaisonDejaObtenu[6]){
+            System.out.print("e chance");
+            nbrPoints = combi.calculerSommeDes();
+            combi.combinaisonDejaObtenu[6] = true;
+        } else {
+            System.out.print("e combinaison déja obtenu");
+        }
+        System.out.println();
+        System.out.println("Vous recevez " + nbrPoints + " points");
+
+        return nbrPoints;
+    }
+
     public static void main(String[] args) {
         afficheDebutJeu();
 
